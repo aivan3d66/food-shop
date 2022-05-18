a2f1764b1491a8a5dc3f0cc04f90cf20761ba567
Ivan <adamovskij00@mail.ru>
2022-05-18 23:23:41 +0300

    add Footer markup styles

--- src/components/UI/Footer/component.js
+++ src/components/UI/Footer/component.js
@@ -1,5 +1,28 @@
 import styled from 'styled-components'
 
 export const Footer = styled.div`
+  width: 100%;
+  padding: 0;
+  margin: 0;
+`
 
+export const FooterWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  max-width: 1248px;
+  min-height: 100px;
+  margin: 0 auto;
+  border: 1px solid red;
+  
+  ul {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 160px;
+    border: 1px solid green;
+    
+    li {
+      border: 1px solid blue;
+    }
+  }
 `
--- src/components/UI/Footer/index.tsx
+++ src/components/UI/Footer/index.tsx
@@ -1,9 +1,16 @@
-import { Footer } from './component'
+import { Footer, FooterWrapper } from './component'
 
 export default () => {
     return (
         <Footer>
-            Footer
+            <FooterWrapper>
+                <ul>
+                    <li>VK</li>
+                    <li>FB</li>
+                    <li>TW</li>
+                    <li>OK</li>
+                </ul>
+            </FooterWrapper>
         </Footer>
     )
 }
