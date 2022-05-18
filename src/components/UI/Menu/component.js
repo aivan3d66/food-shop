6164ce37f63c7753acda6213fef42cb87644a48a
Ivan <adamovskij00@mail.ru>
2022-05-18 23:23:48 +0300

    add Menu markup styles

--- src/components/UI/Menu/component.js
+++ src/components/UI/Menu/component.js
@@ -1,5 +1,36 @@
 import styled from 'styled-components'
 
 export const Menu = styled.div`
+  width: 100%;
+  padding: 0;
+  margin: 0;
+`
 
+export const MenuWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  grid-template-rows: auto;
+  grid-gap: 20px;
+  max-width: 1248px;
+  min-height: 100vh;
+  margin: 0 auto;
+  padding: 40px 0;
+  font-size: 16px;
+  border: 1px solid red;
+  
+  ul {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid blue;
+  }
+`
+
+export const MenuLogo = styled.div`
+  width: 227px;
+  height: 227px;
+  margin: auto;
+  background-color: #cacaca;
+  border-radius: 50%;
 `
--- src/components/UI/Menu/index.tsx
+++ src/components/UI/Menu/index.tsx
@@ -1,10 +1,39 @@
-import { Menu } from './component'
+import { Menu, MenuLogo, MenuWrapper } from './component'
+import { useId } from 'react'
 
 
 export default () => {
+    const data: Array<string> = ['subsection', 'subsection', 'subsection', 'subsection', 'subsection', 'subsection', 'subsection']
+
     return (
         <Menu>
-            Menu
+            <MenuWrapper>
+                <ul>
+                    Section
+                    {
+                        data.map(m => <li key={useId()}>{m}</li>)
+                    }
+                </ul>
+                <ul>
+                    Section
+                    {
+                        data.map(m => <li key={useId()}>{m}</li>)
+                    }
+                </ul>
+                <MenuLogo/>
+                <ul>
+                    Section
+                    {
+                        data.map(m => <li key={useId()}>{m}</li>)
+                    }
+                </ul>
+                <ul>
+                    Section
+                    {
+                        data.map(m => <li key={useId()}>{m}</li>)
+                    }
+                </ul>
+            </MenuWrapper>
         </Menu>
     )
 }
