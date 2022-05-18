8c0df29b43f0fc380e430b7464d75f29c7504dd9
Ivan <adamovskij00@mail.ru>
2022-05-18 22:16:23 +0300

    add Header component

---
+++ src/components/UI/Header/index.tsx
@@ -0,0 +1,14 @@
+import { Header } from './component'
+
+export default () => {
+    return (
+        <Header>
+            <div>
+                Menu
+            </div>
+            <div>
+                Basket
+            </div>
+        </Header>
+    )
+}
