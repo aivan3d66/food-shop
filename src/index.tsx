d46c8d785a52bf73fb9e1fc4c2eab361128a8f9a
Ivan <adamovskij00@mail.ru>
2022-05-18 22:25:34 +0300

    delete css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import reportWebVitals from './reportWebVitals'
 import AppRouter from './components/BLL/AppRouter'
 import { BrowserRouter } from 'react-router-dom'
