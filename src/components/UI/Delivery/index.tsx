f92609a5dd0c4d5a1360e045bf97ee23683eb641
Ivan <adamovskij00@mail.ru>
2022-05-19 22:14:00 +0300

    add upd delivery callbacks

--- src/components/UI/Delivery/index.tsx
+++ src/components/UI/Delivery/index.tsx
@@ -2,9 +2,21 @@ import { Delivery, DeliveryBody, DeliveryControls, DeliveryHeader, DeliveryWrapp
 import Button from '../Button/Button'
 import InputText from '../InputText/InputText'
 import { useState } from 'react'
+import { useActions } from '../../../utils/helpers'
+import { testActions } from '../../../state/slices/testSlice'
 
 export default () => {
     const [deliveryState, setDeliveryState] = useState<boolean>(true)
+    const { changeDeliveryToggle } = useActions({ ...testActions })
+
+    const setDeliveryHandler = () => {
+        setDeliveryState(true)
+        changeDeliveryToggle({ deliveryFilter: 'all' })
+    }
+    const setPickUpHandler = () => {
+        setDeliveryState(false)
+        changeDeliveryToggle({ deliveryFilter: 'delivery' })
+    }
 
     return (
         <Delivery>
@@ -12,8 +24,8 @@ export default () => {
                 <DeliveryHeader>
                     <h1>Delivery in Moscow city</h1>
                     <DeliveryControls>
-                        <Button deliveryState={deliveryState} onClick={() => setDeliveryState(true)} name={'Delivery'} />
-                        <Button deliveryState={!deliveryState} onClick={() => setDeliveryState(false)} name={'Pick up'} />
+                        <Button deliveryState={deliveryState} onClick={setDeliveryHandler} name={'Delivery'} />
+                        <Button deliveryState={!deliveryState} onClick={setPickUpHandler} name={'Pick up'} />
                     </DeliveryControls>
                 </DeliveryHeader>
                 {
