670f4d94222214f86a8aadb9a3d02dee1bce01ee
Ivan <adamovskij00@mail.ru>
2022-05-19 09:43:00 +0300

    add Button default styles

--- src/components/UI/Button/component.js
+++ src/components/UI/Button/component.js
@@ -1,4 +1,17 @@
 import styled from 'styled-components'
 
 export const Button = styled.button`
+  min-width: 100px;
+  padding: 12px 14px;
+  color: #ffffff;
+  font-size: 16px;
+  line-height: 24px;
+  background-color: #E4002B;
+  border: 1px solid transparent;
+  cursor: pointer;
+  transition: 0.3s;
+  
+  &:hover {
+    background-color: #b30000;
+  }
 `
