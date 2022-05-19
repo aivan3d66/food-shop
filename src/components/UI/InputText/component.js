e62901036a67c00d4ad74258bf2f9e719f33fa99
Ivan <adamovskij00@mail.ru>
2022-05-19 09:43:13 +0300

    add InputText default styles

--- src/components/UI/InputText/component.js
+++ src/components/UI/InputText/component.js
@@ -1,4 +1,18 @@
 import styled from 'styled-components'
 
 export const InputText = styled.input`
+  padding: 10px;
+  margin: 0 10px;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 140%;
+  color: #9D9D9D;
+  background-color: #F7F6F5;
+  border: 1px solid transparent;
+  border-radius: 4px;
+  transition: 0.3s;
+  
+  &:hover {
+    border-color: #9D9D9D;
+  }
 `
