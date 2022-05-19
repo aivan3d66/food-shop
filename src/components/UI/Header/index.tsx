9aaa9f1254b8cf668900a11c7bfd318d8d7dde82
Ivan <adamovskij00@mail.ru>
2022-05-19 09:14:49 +0300

    add HeaderTricolor, Menu and Basket blocks

--- src/components/UI/Header/index.tsx
+++ src/components/UI/Header/index.tsx
@@ -1,15 +1,27 @@
-import { Header, HeaderWrapper } from './component'
+import { Basket, Header, HeaderMenu, HeaderTricolor, HeaderWrapper } from './component'
+import { HiMenuAlt1 } from 'react-icons/hi'
+import { useAppSelector } from '../../../utils/helpers'
+import basket from './../../../assets/images/basket.svg'
 
 export default () => {
+    const { basketPrice } = useAppSelector((state) => state.testReducer)
+
     return (
         <Header>
+            <HeaderTricolor>
+                <span></span>
+                <span></span>
+                <span></span>
+            </HeaderTricolor>
+
             <HeaderWrapper>
-                <div>
-                    Menu
-                </div>
-                <div>
-                    Basket
-                </div>
+                <HeaderMenu>
+                    <HiMenuAlt1 />
+                </HeaderMenu>
+                <Basket>
+                    {basketPrice} &#8381;
+                    <img src={basket} alt='basket icon' />
+                </Basket>
             </HeaderWrapper>
         </Header>
     )
