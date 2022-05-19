dadfa65bc4716412eb1c481678b8294acdabab6c
Ivan <adamovskij00@mail.ru>
2022-05-19 15:27:52 +0300

    add theme file

---
+++ src/theme.js
@@ -0,0 +1,28 @@
+const black = '#000000'
+const darkGray = '#9D9D9D'
+const lightGray = '#F7F6F5'
+const white = '#ffffff'
+const red = '#E4002B'
+const darkRed = '#b30000'
+
+const buttonIsActive = {
+    textColor: white,
+    background: red,
+}
+const buttonNotActive = {
+    textColor: darkGray,
+    background: lightGray,
+}
+
+export default  {
+    buttonIsActive,
+    buttonNotActive,
+    colors: {
+        black,
+        darkGray,
+        lightGray,
+        white,
+        red,
+        darkRed,
+    }
+}
