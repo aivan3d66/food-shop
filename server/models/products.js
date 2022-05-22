6bfe5a11462b43954574753ed3e06ae610374c76
Ivan <adamovskij00@mail.ru>
2022-05-22 17:38:09 +0300

    add upd product model

--- server/models/products.js
+++ server/models/products.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const productsSchema = new Schema({
-    id: String,
     name: String,
-    popularity: Array,
+    popularity: String,
     image: String,
     price: Number,
     type: String,
