d3f7e9bd58888ef0b716123a979c3adf5655fedd
Ivan <adamovskij00@mail.ru>
2022-05-26 18:43:41 +0300

    add Error component

--- src/components/UI/Goods/index.tsx
+++ src/components/UI/Goods/index.tsx
@@ -1,8 +1,8 @@
-import { Goods, GoodsWrapper, GoodsCategory } from './component'
+import { Goods, GoodsWrapper, GoodsCategory, Error } from './component'
 import { useActions, useAppSelector } from '../../../utils/helpers'
 import GoodCard from './GoodCard'
 import { GoodType, testActions } from '../../../state/slices/testSlice'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Preloader from '../Preloader'
 import { client } from '../../../index'
 import { productsQuery } from './queries'
@@ -10,11 +10,16 @@ import { productsQuery } from './queries'
 export default () => {
     const { getProducts } = useActions({ ...testActions })
     const { deliveryFilter, products } = useAppSelector((state) => state.testReducer)
+    const [appError, setAppError] = useState<string>('kek')
 
     useEffect(() => {
-        client.query({ query: productsQuery }).then((res: any) => {
-            getProducts({ products: res.data?.products ?? [] })
-        })
+        client.query({ query: productsQuery })
+            .then((res: any) => {
+                getProducts({ products: res.data?.products ?? [] })
+            })
+            .catch((error: any) => {
+                setAppError(error.message)
+            })
 
     }, [getProducts])
 
@@ -26,76 +31,82 @@ export default () => {
 
     return (
         <Goods>
-            <GoodsWrapper>
-                <GoodsCategory id='BurgersControl'>
-                    <h2>Burgers</h2>
-                    {
-                        products.length === 0
-                            ? <Preloader />
-                            : <>
-                                <ul>
-                                    {
-                                        copyGoods.filter((f: any) => f.type === 'burger').map((m: any) => {
-                                            return <GoodCard
-                                                key={m.id}
-                                                name={m.name}
-                                                image={m.image}
-                                                popularity={m.popularity}
-                                                price={m.price}
-                                            />
-                                        })
-                                    }
-                                </ul>
-                            </>
-                    }
-                </GoodsCategory>
-            </GoodsWrapper>
-            <GoodsCategory id='TwistersControl'>
-                <h2>Twisters</h2>
-                {
-                    products.length === 0
-                        ? <Preloader />
-                        : <>
-                            <ul>
+            {
+                appError
+                    ? <Error>Houston, we have a problem! Perhaps the problem with this: <span>{appError}</span></Error>
+                    : <>
+                        <GoodsWrapper>
+                            <GoodsCategory id='BurgersControl'>
+                                <h2>Burgers</h2>
                                 {
-                                    copyGoods.filter(f => f.type === 'twister').map((m) => {
-                                        return <GoodCard
-                                            key={m.id}
-                                            name={m.name}
-                                            image={m.image}
-                                            popularity={m.popularity}
-                                            price={m.price}
-                                        />
-                                    })
+                                    products.length === 0
+                                        ? <Preloader />
+                                        : <>
+                                            <ul>
+                                                {
+                                                    copyGoods.filter((f: any) => f.type === 'burger').map((m: any) => {
+                                                        return <GoodCard
+                                                            key={m.id}
+                                                            name={m.name}
+                                                            image={m.image}
+                                                            popularity={m.popularity}
+                                                            price={m.price}
+                                                        />
+                                                    })
+                                                }
+                                            </ul>
+                                        </>
                                 }
-                            </ul>
-                        </>
-                }
-            </GoodsCategory>
-            <GoodsWrapper>
-                <GoodsCategory id='ChickenControl'>
-                    <h2>Chicken</h2>
-                    {
-                        products.length === 0
-                            ? <Preloader />
-                            : <>
-                                <ul>
-                                    {
-                                        copyGoods.filter(f => f.type === 'strips').map((m) => {
-                                            return <GoodCard
-                                                key={m.id}
-                                                name={m.name}
-                                                image={m.image}
-                                                popularity={m.popularity}
-                                                price={m.price}
-                                            />
-                                        })
-                                    }
-                                </ul>
-                            </>
-                    }
-                </GoodsCategory>
-            </GoodsWrapper>
+                            </GoodsCategory>
+                        </GoodsWrapper>
+                        <GoodsCategory id='TwistersControl'>
+                            <h2>Twisters</h2>
+                            {
+                                products.length === 0
+                                    ? <Preloader />
+                                    : <>
+                                        <ul>
+                                            {
+                                                copyGoods.filter(f => f.type === 'twister').map((m) => {
+                                                    return <GoodCard
+                                                        key={m.id}
+                                                        name={m.name}
+                                                        image={m.image}
+                                                        popularity={m.popularity}
+                                                        price={m.price}
+                                                    />
+                                                })
+                                            }
+                                        </ul>
+                                    </>
+                            }
+                        </GoodsCategory>
+                        <GoodsWrapper>
+                            <GoodsCategory id='ChickenControl'>
+                                <h2>Chicken</h2>
+                                {
+                                    products.length === 0
+                                        ? <Preloader />
+                                        : <>
+                                            <ul>
+                                                {
+                                                    copyGoods.filter(f => f.type === 'strips').map((m) => {
+                                                        return <GoodCard
+                                                            key={m.id}
+                                                            name={m.name}
+                                                            image={m.image}
+                                                            popularity={m.popularity}
+                                                            price={m.price}
+                                                        />
+                                                    })
+                                                }
+                                            </ul>
+                                        </>
+                                }
+                            </GoodsCategory>
+                        </GoodsWrapper>
+                    </>
+            }
         </Goods>
     )
 }
