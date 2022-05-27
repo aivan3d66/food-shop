97adbd9005bce1139c4a1bff52af5f33499b0461
Ivan <adamovskij00@mail.ru>
2022-05-27 14:02:53 +0300

    add getCategoriesQuery

--- src/components/UI/Filter/index.tsx
+++ src/components/UI/Filter/index.tsx
@@ -1,22 +1,51 @@
 import { Filter, FilterWrapper } from './component'
-import { useAppSelector } from '../../../utils/helpers'
+import { useActions, useAppSelector } from '../../../utils/helpers'
+import { client } from '../../../index'
+import { useEffect } from 'react'
+import { CategoryType, testActions } from '../../../state/slices/testSlice'
+import Preloader from '../Preloader'
+import { categoriesQuery } from './queries'
 
+type CategoriesQueryResponseType = {
+    data: {
+        categories: Array<CategoryType>
+    }
+}
 
 export default () => {
     const { categories } = useAppSelector((state) => state.testReducer)
-    const filters: Array<string> = ['Burgers', 'Twisters', 'Chicken', 'Baskets', 'Snakes', 'Sauce', 'Drinks', 'Tee & Coffee', 'Desserts', 'Hits at 50', 'Hits at 51']
+    const { getCategories, setAppError } = useActions({ ...testActions })
+
+    const getCategoriesQuery = () => {
+        client.query({ query: categoriesQuery })
+            .then((res: CategoriesQueryResponseType) => {
+                getCategories({ categories: res.data?.categories ?? [] })
+            })
+            .catch((error: any) => {
+                setAppError(error.message)
+            })
+    }
+
+    useEffect(() => {
+        getCategoriesQuery()
+    }, [])
 
     return (
         <Filter>
             <FilterWrapper>
-                <ul>
-                    {
-                        filters.map((m, i: number) => {
-                            const scrollId = `#${m}Control`
-                            return <li key={i}><a href={scrollId}>{m}</a></li>
-                        })
-                    }
-                </ul>
+                {
+                    categories.length === 0
+                        ? <Preloader />
+                        : <ul>
+                            {
+                                categories.map((m, i: number) => {
+                                    const scrollId = `#${m}Control`
+                                    return <li key={i}><a href={scrollId}>{m.name}</a></li>
+                                })
+                            }
+                        </ul>
+                }
+
             </FilterWrapper>
         </Filter>
     )
