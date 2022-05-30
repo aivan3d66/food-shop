ee0955dd8b2f259be8041ec27bfed8d194dd2fbd
Ivan <adamovskij00@mail.ru>
2022-05-30 19:46:59 +0300

    add BASKET_PAGE route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Home } from '../pages/Home/Home'
+import Layout from '../components/UI/Layout/Layout'
+import Basket from '../pages/Basket/Basket'
 
 export interface IRoute {
     path: string
@@ -8,11 +10,16 @@ export interface IRoute {
 
 export enum RouteNames {
     START_PAGE = '/',
+    BASKET_PAGE = '/basket-page',
 }
 
 export const routes: IRoute[] = [
     {
         path: RouteNames.START_PAGE,
-        component: <Home />,
+        component: <Layout><Home /></Layout>,
+    },
+    {
+        path: RouteNames.BASKET_PAGE,
+        component: <Layout><Basket /></Layout>,
     },
 ]
