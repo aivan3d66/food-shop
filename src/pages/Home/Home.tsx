0489d53aea96c252131f8289ff9928b3324ded3b
Ivan <adamovskij00@mail.ru>
2022-05-30 19:47:41 +0300

    add upd Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,19 +1,15 @@
-import Header from '../../components/UI/Header'
 import Delivery from '../../components/UI/Delivery'
 import Filter from '../../components/UI/Filter'
 import Goods from '../../components/UI/Goods'
 import Menu from '../../components/UI/Menu'
-import Footer from '../../components/UI/Footer'
 
 export const Home = () => {
     return (
         <>
-            <Header />
             <Delivery />
             <Filter />
             <Goods />
             <Menu />
-            <Footer />
         </>
     )
 }
