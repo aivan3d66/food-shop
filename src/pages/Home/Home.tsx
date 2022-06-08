ea140c740fb9e1409e6dee3213934b80aefabdd6
Ivan <adamovskij00@mail.ru>
2022-06-08 17:39:33 +0300

    add observer state and handlers

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,37 @@
 import Delivery from '../../components/UI/Delivery'
 import Filter from '../../components/UI/Filter'
-import Goods from '../../components/UI/Goods'
 import Menu from '../../components/UI/Menu'
+import { createRef, useEffect, useState } from 'react'
+import { useAppSelector } from '../../utils/helpers'
+import ProductLists from '../../components/UI/Goods/ProductLists'
 
 export const Home = () => {
+    const [activeCategory, setActiveCategory] = useState()
+    const [pageHeight, setPageHeight] = useState<number>(0)
+
+    const { categories } = useAppSelector((state) => state.shopPageReducer)
+
+    const refs = categories.reduce((refsObj: any, category) => {
+        refsObj[category.name] = createRef()
+        return refsObj
+    }, {})
+
+    const handleCLick = (name: string) => {
+        refs[name].current.scrollIntoView({
+            behavior: 'smooth',
+            block: 'center',
+        })
+    }
+
+    useEffect(() => {
+        setPageHeight(window.innerHeight)
+        window.addEventListener('resize', (e) => {
+            setTimeout(() => {
+                setPageHeight(window.innerHeight)
+            }, 300)
+        })
+    }, [])
+
     return (
         <>
             <Delivery />
