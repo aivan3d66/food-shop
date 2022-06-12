0545ecdefd8b1de333b8f835a811236662fb90d6
Ivan <adamovskij00@mail.ru>
2022-06-12 22:13:21 +0300

    add upd Basket styles

--- src/components/UI/Basket/component.js
+++ src/components/UI/Basket/component.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const Basket = styled.div`
   position: relative;
   width: 100%;
+  min-height: 100vh;
   padding: 20px;
   margin: 0;
 `
@@ -21,39 +22,6 @@ export const BasketProducts = styled.div`
   ul {
     width: 100%;
     padding: 20px 0;
-
-    li {
-      display: flex;
-      justify-content: space-between;
-      padding: 20px 0;
-      align-items: center;
-      font-weight: 400;
-      font-size: 18px;
-      line-height: 32px;
-      
-      span {
-        color: #E4002B;
-      }
-
-      @media (min-width: 768px) {
-        div {
-          display: flex;
-          justify-content: space-between;
-          min-width: 300px;
-          margin: 0 auto;
-        }
-      }
-      
-      @media (min-width: 768px) {
-        font-size: 24px;
-      }
-    }
-
-    img {
-      width: 120px;
-      height: auto;
-      margin: 0;
-    }
   }
 `
 
@@ -63,31 +31,32 @@ export const BasketEmpty = styled.span`
 `
 
 export const BasketPrice = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
-
-  span {
-    margin: 40px 0;
-    font-weight: 600;
-    font-size: 24px;
-    line-height: 32px;
-  }
-
+  
   button {
     margin: 40px 40px 40px 0;
   }
 `
 
+export const Price = styled.span`
+    margin: 40px 0;
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 32px;
+`
+
 export const ValidationMessage = styled.span`
   position: absolute;
-  bottom: -60px;
+  bottom: -20px;
   width: auto;
   padding: 12px 20px;
   color: #ffffff;
   text-align: center;
   font-weight: 700;
-  font-size: 18px;
-  line-height: 22px;
+  font-size: 14px;
+  line-height: 16px;
   background-color: #000000;
   border-radius: 8px;
   transition: 0.3s;
@@ -96,10 +65,16 @@ export const ValidationMessage = styled.span`
     position: absolute;
     content: '';
     top: -5px;
-    left: 50%;
+    left: 70%;
+    right: 50%;
     width: 14px;
     height: 14px;
     background-color: #000000;
     transform: rotate(45deg);
+  }
+
+  @media (min-width: 768px) {
+    font-size: 18px;
+    line-height: 22px;
   }
 `
